a65b7ffb1f45b91f4cf5688c3944cad6829693ff
amahra2 <aadarsh.mahra@gmail.com>
2021-11-06 18:41:53 -0500

    signup form added

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from forms import SignUpForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thecodex'
 
-@app.route('/')
+@app.route('/home')
 def home():
-    return 'Hello World'
+    return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
 if __name__ == '__main__':
     app.run()
 
