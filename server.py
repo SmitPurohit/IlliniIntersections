7e2909304cc47d4ee1d2748287f5c92dddef6f03
amahra2 <aadarsh.mahra@gmail.com>
2021-11-08 15:49:36 -0600

    reorganized utility functions, started intersection read, added frontend

--- server.py
+++ server.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from forms import SignUpForm
+from utils.functions_back import get_intersection_info
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thecodex'
 
-@app.route('/home')
+
+@app.route('/home', methods=['POST', 'GET'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        street_ew = request.form['nm']
+        street_ns = request.form['ew']
+        print(street_ew, street_ns)
+    else:
+        return render_template('index.html')
+
 
 @app.route('/signup')
 def signup():
     form = SignUpForm()
     return render_template('signup.html', form=form)
+
+
 if __name__ == '__main__':
     app.run()
 
---
+++ utils/functions_back.py
@@ -0,0 +1,76 @@
+import sqlite3
+from google.cloud import storage
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import mysql.connector
+import sys
+
+
+def main():
+    with open('../config.txt', 'r') as f:
+        HOST = f.readline().strip()
+        USER = f.readline().strip()
+        PASSWORD = f.readline().strip()
+        DATABASE = f.readline().strip()
+
+    mydb = mysql.connector.connect(host=HOST,
+                                   user=USER,
+                                   password=PASSWORD,
+                                   database=DATABASE)
+    # advanced_queries(mydb)
+    get_intersection_info(mydb, 'Gary Highway', 'Neal Shore')
+
+
+# return the intersection info for these two streets
+def get_intersection_info(database, ew_name, ns_name):
+    # Returns streetID
+    streetEW_q = (f"SELECT streetid "
+                 f"FROM Street "
+                 f"WHERE name = '{ew_name}'")
+    # Returns streetID
+    streetNS_q = (f"SELECT streetid " 
+                 f"FROM Street "
+                 f"WHERE name = '{ns_name}'")
+
+    intersectionID_q = (f"SELECT intersectionID "
+                 f"FROM Intersection "
+                 f"WHERE streetEWID = ({streetEW_q}) AND streetNSID = ({streetNS_q})")
+
+    intersectionData_q = (f"SELECT intersectionID, comments, overallRating, visualAppeal "
+                        f"FROM Intersection NATURAL JOIN Reviews "
+                        f"WHERE intersectionID = ({intersectionID_q})")
+
+
+    cursor = database.cursor()
+    cursor.execute(intersectionData_q)
+    result = cursor.fetchall()
+    for x in result:
+        print(x)
+    intersectionID, comments, overallRating, visualAppeal = result[0]
+    return intersectionID, comments, overallRating, visualAppeal
+
+
+
+def advanced_queries(database):
+    subquery1 = ("SELECT streetEWID "
+                 "FROM Statistics s JOIN Intersection i on s.statisticsID = i.intersectionID "
+                 "WHERE lightingRating >= 8.0")
+    subquery2 = ("SELECT streetNSID "
+                 "FROM Statistics s JOIN Intersection i on s.statisticsID = i.intersectionID "
+                 "WHERE lightingRating >= 8.0")
+
+    cursor = database.cursor()
+    cursor.execute(
+        f"SELECT name, direction FROM Street "
+        f"WHERE streetID IN ({subquery1} UNION {subquery2}) "
+        f"GROUP BY name, direction "
+        f"LIMIT 15; "
+    )
+    result = cursor.fetchall()
+    for x in result:
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
